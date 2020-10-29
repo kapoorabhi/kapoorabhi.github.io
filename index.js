38715b3694aa9347f0e9a7fbd0b57d1bfc6486fc
Abhishek Kapoor <kapoor.abhi@outlook.com>
2020-10-28 21:11:16 -0400

    added basic service worker config

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('yo');
+console.log('In Index file');
+
+if('serviceWorker' in navigator) {
+    console.log('CLIENT::: Service Worker registration in progress!');
+    navigator.serviceWorker.register('/.service-worker.js').then(() => {
+        console.log('CLIENT:: Service Worker registration complete..Yayy!!');
+    }, () => {
+        console.log('CLIENT:: Service Worker registration failed..Noo :(');
+    })
+}
+else {
+    console.log('CLIENT::: Service Worker is not supported :(');
+}
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+let version = 'v1::';
+
+self.addEventListener('install', function(event) {
+    console.lof('WORKER:: install event in progress!!');
+
+    event.waitUntil(
+        /*
+            The caches built-in is a Promise based API that helps you cache responses,
+            as well as inding and deleting them
+        */
+       caches
+        /*
+            You can open a cache by name and this method returns a promise.
+            We use a versioned cache name here so that we can remove old cache entries 
+            in one fell swoop later, when phasing out an older service worker
+        */
+       .open(`${version}fundamentals`)
+       .then(function(cache) {
+           console.log('I am the cache::', cache);
+            /*
+                After the cache is opened, we can fill it with the offline fundamentals.
+                The method below will add all resources we have indicated to the cache,
+                after making HTTP requests for each of them
+            */
+           return cache.addAll([
+               '/',
+               'index.css',
+               'index.js'
+           ]);
+       })
+       .then(function() {
+           console.log('WORKER:: Install Completed!!');
+       })
+    );
+});
